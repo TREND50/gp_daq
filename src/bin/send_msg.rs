9b7a41685f38fdb632b9b9f2b47b536e02b5c1a2
Gu Junhua <jhgu@nao.cas.cn>
2018-08-31 20:16:14 +0800

    modified:   src/bin/send_msg.rs

--- src/bin/send_msg.rs
+++ src/bin/send_msg.rs
@@ -13,22 +13,29 @@ use std::env;
 use std::net::{UdpSocket};
 
 fn main() {
+    let args:Vec<_>=env::args().collect();
 
-    let mut f = File::open(env::args().nth(1).unwrap()).unwrap();
-    let addr=env::args().nth(2).unwrap();
+    if args.len()!=3{
+        eprintln!("Usage: {} <cfg file> <addr:port>", args[0]);
+        return;
+    }
+
+
+    let mut f = File::open(env::args().nth(1).unwrap()).expect("Cannot open file");
+    let addr=env::args().nth(2).expect("Invalid addr");
 
     let mut bytes=Vec::new();
-    f.read_to_end(&mut bytes).unwrap();
+    f.read_to_end(&mut bytes).expect("Cannot read file");
     let msg_str=str::from_utf8(&bytes).unwrap().to_string();
-
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("bind failed");
     for s in msg_str.split("---"){
         let _=from_str::<Value>(s).map(|v|{
             //println!("{:?}", v);
             let daq1 = gp_daq::msg::TrendMsg::from_yaml(&v);
             let data = daq1.to_byte_vec();
             println!("sending msg with size {}",data.len());
-            let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
-            let _=socket.send_to(&data[..], addr.clone());
+
+            let _=socket.send_to(&data[..], addr.clone()).expect("send data failed");
         });
     }
 }
