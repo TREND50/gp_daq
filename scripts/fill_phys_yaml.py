2cbdb799919b339d725d162c155e1eea7749ccb2
Gu Junhua <jhgu@nao.cas.cn>
2018-09-12 20:34:27 +0800

    modified:   scripts/fill_phys_yaml.py

--- scripts/fill_phys_yaml.py
+++ scripts/fill_phys_yaml.py
@@ -4,13 +4,13 @@ import yaml
 import sys
 
 data=[i for i in yaml.load_all(open(sys.argv[1]).read())]
-print(data[9])
-data[9]['th1m']=sys.argv[2]
-data[9]['th1p']=sys.argv[3]
-data[9]['th2m']=sys.argv[4]
-data[9]['th2p']=sys.argv[5]
-data[9]['th3m']=sys.argv[6]
-data[9]['th3p']=sys.argv[7]
+
+data[9]['th1m']=int(sys.argv[2])
+data[9]['th1p']=int(sys.argv[3])
+data[9]['th2m']=int(sys.argv[4])
+data[9]['th2p']=int(sys.argv[5])
+data[9]['th3m']=int(sys.argv[6])
+data[9]['th3p']=int(sys.argv[7])
 data[9]['trg_en']=int(sys.argv[8],2)
 print(int(sys.argv[8],2))
 yaml.safe_dump_all(data, open(sys.argv[9], 'w'))
