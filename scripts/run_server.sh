aa0413ec5ebfd074eddf46c159bd6b798ed9cc74
Gu Junhua <jhgu@nao.cas.cn>
2019-01-16 11:22:18 +0800

    modified:   scripts/run_server.sh

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -41,5 +41,7 @@ fi
 
 tmux select-pane -t 0
 echo "Now starting server."
-tmux send-keys "$PROG_DIR/trend_server 0.0.0.0 ${SLC_PORT} ${DATA_PORT} 8888 $DUMP_FILE" C-m
+tmux send-keys "cargo run -q --manifest-path $PROG_DIR/Cargo.toml --bin \
+    trend_server --release -- -a 0.0.0.0 -s ${SLC_PORT} -d ${DATA_PORT} -m 8888 -t ${DUMP_FILE}.yaml -b ${DUMP_FILE}.bin -c ${DUMP_FILE}_data.yaml -v 1"  C-m
+#tmux send-keys "$PROG_DIR/trend_server 0.0.0.0 ${SLC_PORT} ${DATA_PORT} 8888 $DUMP_FILE" C-m
 #sleep .5  # Needed on laptop
