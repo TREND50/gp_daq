c9e42b8971f71cfb6eb13e9f92eb5eae8c7cef8a
Gu Junhua <jhgu@nao.cas.cn>
2019-04-24 01:05:58 +0800

    deleted:    scripts/set_addr.sh

--- scripts/set_addr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $# -lt 2 ]
-then
-    echo "Usage: " $0 "<ifname> <macfile>"
-    exit
-fi
-./gen_ip_cfg.sh $1 $2 >addr.yaml 
-sudo ../target/release/set_addr $1 ./addr.yaml 1234 8888
