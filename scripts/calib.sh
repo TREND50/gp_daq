0e0f18320c3634d06fcabb87901a3081253f8c34
Olivier Martineau <omartino@in2p3.fr>
2019-05-15 21:00:15 +0200

    Fixed bugs on calib scripts

--- scripts/calib.sh
+++ scripts/calib.sh
@@ -31,30 +31,33 @@ NRUN=$(<$DATADIR/last_run.txt)
 NRUN=$(($NRUN+1))
 echo "NRUN=" $NRUN
 
-if [ "$2" -gt "127" ] || [ "$3" -gt "127" ]
+if [ "$1" -gt "127" ] || [ "$2" -gt "127" ]
 then
   echo "Error: 127 is the max value allowed for attenuation factor. Aborting."
   exit 0
 fi
   
-awk '/Attr1/{$3='$2'}1;' $CFG_DIR/calibgen.yaml > $CFG_DIR/temp.yaml
-awk '/Attr2/{$3='$3'}1;'  $CFG_DIR/temp.yaml > $CFG_DIR/calib$NRUN.yaml
+awk '/att1/{$2='$1'}1;' $CFG_DIR/calibgen.yaml > $CFG_DIR/temp.yaml
+awk '/att2/{$2='$2'}1;'  $CFG_DIR/temp.yaml > $CFG_DIR/calib$NRUN.yaml
 
 # Clean
-tmux kill-window -t "calib"  
+tmux kill-window -t "calib" 
+ 
+
+# Log run id
+rm $DATADIR/last_run.txt
+echo $NRUN >> $DATADIR/last_run.txt
+cp ${CFG_DIR}/calib$NRUN.yaml  $DATADIR/C$NRUN.yaml
+
 # Execute run
 SLC_FILE=$DATADIR/S${NRUN}.yaml
 DATA_FILE=$DATADIR/C${NRUN}.data
-
 cat boardsIn.txt | while read BOARDID
 do
   echo "Now doing board " $BOARDID
   $SELF_DIR/run.sh  ${BOARDID} $CFG_DIR/calib$NRUN.yaml 1235 $SLC_FILE 1236 $DATA_FILE "calib" 500
 done
 
-# Log run id
-cp ${CFG_DIR}/calib$NRUN.yaml  $DATADIR/M$NRUN.yaml
-rm $DATADIR/last_run.txt
-echo $NRUN >> $DATADIR/last_run.txt
+# Clean
 echo "Now killing tmux window calib." 
 tmux kill-window -t "calib"
